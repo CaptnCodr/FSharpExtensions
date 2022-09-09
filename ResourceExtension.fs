d5e119b4036c3c96aaf886467df3c6e5399790fd
Constantin Tews <constantin.tews@gmail.com>
2022-09-09 14:22:42 +0200

    Add ResourceExtension.fs.

---
+++ ResourceExtension.fs
@@ -0,0 +1,23 @@
+﻿namespace Extensions
+
+open System.Reflection
+open System.Resources
+
+type ResourceExtension (resourceFile) =
+
+    member this.GetResourceString ty = 
+        ty |> string |> ResourceManager(resourceFile, Assembly.GetExecutingAssembly()).GetString
+
+    member this.GetFormattedString (ty, [<System.ParamArray>]args : obj array) = 
+         (this.GetResourceString ty, args) |> System.String.Format
+
+module ResourceExt =
+
+    let resourceManager (file: string) =
+        ResourceManager(file, Assembly.GetExecutingAssembly())
+
+    let getResourceString ty (resourceManager: ResourceManager) =
+        ty |> string |> resourceManager.GetString
+
+    let getFormattedString ty ([<System.ParamArray>]args : obj array) (resourceManager: ResourceManager) =
+        (ty |> string |> resourceManager.GetString, args) |> System.String.Format
